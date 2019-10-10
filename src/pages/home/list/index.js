e69f98d5e7d9458ae2ad65056d15142b43dd7997
yuchuan <825077792@qq.com>
2019-10-10 11:50:10 +0800

    list

--- src/pages/home/list/index.js
+++ src/pages/home/list/index.js
@@ -3,11 +3,12 @@ import React, { Component } from 'react';
 import dva, {connect} from 'dva';
 import Link from 'umi/link';
 
-import { Sticky,Tabs, PullToRefresh,WhiteSpace } from 'antd-mobile';
+import { Sticky,Tabs, PullToRefresh,WhiteSpace ,Drawer} from 'antd-mobile';
 import Masonry from 'react-masonry-component';
 import dataList from './dadta';
 import DiscItem from '@/components/discoveryItem'
 import SearchCard from '@/components/searchCard'
+import defaultImg from './6.jpg';
 
 
 import request from 'umi-request';
@@ -26,86 +27,103 @@ const masonryOptions = {
   count
 }))
 
-class Discovery extends Component {
+class BussList extends Component {
   constructor(props) {
     super(props);
     this.state = {
       tabs: [],
       dataList: [],
       handleOnFresh: false,
-      activeType:0
+      activeType:0,
+      open:true,
+      activeFirList:0,
+      activeSecList:0,
+      navData:[
+        {
+          lable:'美食1',
+          number:100,
+          children:[{
+            lable:'美食1-1',
+            number:10,
+            imgUrl:defaultImg
+          },{
+            lable:'美食1-1',
+            number:10,
+            imgUrl:defaultImg
+            
+          },{
+            lable:'美食1-1',
+            number:10,
+            imgUrl:defaultImg
+          },{
+            lable:'美食1-1',
+            number:10,
+            imgUrl:defaultImg
+          }]
+        },{
+          lable:'美食2',
+          number:100,
+          children:[{
+            lable:'美食2-1',
+            number:10,
+            imgUrl:defaultImg
+          },{
+            lable:'美食2-1',
+            number:10,
+            imgUrl:defaultImg
+          },{
+            lable:'美食2-1',
+            number:10,
+            imgUrl:defaultImg
+          },{
+            lable:'美食2-1',
+            number:10,
+            imgUrl:defaultImg
+          },{
+            lable:'美食2-1',
+            number:10,
+            imgUrl:defaultImg
+          }]
+        }
+      ]
     }
-    this.handleOnFresh = this.handleOnFresh.bind(this);
-    this.getFindType = this.getFindType.bind(this);
     this.handelTabClick = this.handelTabClick.bind(this);
+    this.handleFirNavClick = this.handleFirNavClick.bind(this);
+    this.handleSecNavClick = this.handleSecNavClick.bind(this);
+    this.getStoreClassList = this.getStoreClassList.bind(this);
   }
 
-
-  onScrollStart() {
-    console.log('start');
-  }
   componentDidMount() {
-    this.getFindType();
-    this.getAttentionList();
+    this.getStoreClassList();
   }
   componentDidUpdate(prevProps,prevState){
     
-    if(this.state.activeType!==prevState.activeType){
-      //请求新列表
-      this.getAttentionList();
-    }
   }
-
-  getFindType(refresh){
-   if(this.state.tabs.length && !refresh) return;
-   API.findType({
-      type:'find'
-    }).then(res=>{
-      let tabs=[];
-      res.forEach(el => {
-        tabs.push({
-          title:el.name,
-          type:el.type
-        })
-      });
-
-      this.setState({
-        tabs:tabs,
+  getRecList(data){
+    let tabs=[];
+    data.forEach(el => {
+      tabs.push({
+        title:el.name,
+        id:el.id
       })
-      console.log(this.state.tabs);
-    }).catch(err=>{
-      console.log(err);
+    });
+
+    this.setState({
+      tabs:tabs,
     })
   }
-  getAttentionList(){
-    let para ={
-      type:this.state.activeType,
-      user_id:this.state.user_id,
-      lon:this.state.lon,
-      lat:this.state.lat,
-      page:this.state.currentPage,
-      pageSize:this.state.currentPageSize
+  getStoreClassList(){
+    let params={
+      type:'food',
+      area_id:20
     }
-    let data =  [{
-        "id": 1,
-        "title": "string",
-        "cover_image": "string",
-        "author_avatar": "string",
-        "author_name": "string",
-        "pageview": 0,
-        "is_like": 0
-      },{
-        "id": 2,
-        "title": "string",
-        "cover_image": "string",
-        "author_avatar": "string",
-        "author_name": "string",
-        "pageview": 0,
-        "is_like": 0
-      }];
+    API.getStoreClassList(params).then(res=>{
+      console.log(res);
+      this.getRecList(res.recommend)
       this.setState({
-        dataList:data
-      })
+        navData:res.list
+      });
+    })
   }
   handelTabClick(tab,index){
     this.setState({
@@ -119,11 +137,30 @@ class Discovery extends Component {
       this.setState({ refreshing: false });
     }, 1000);
   }
-
+  onOpenChange = (...args) => {
+    console.log(args);
+    this.setState({ open: !this.state.open });
+  }
+  handleFirNavClick(index){
+    console.log(index);
+    this.setState({
+      activeFirList:index
+    })
+  }
+  handleSecNavClick(index){
+    console.log(index);
+    
+    this.setState({
+      activeSecList:index
+    })
+  }
   render() {
     return (
-      <div className="page-list page-bg" id="page-list">
-        <div className="bg-color-theme padding-left-15 padding-right-15 border-box top-wrapper padding-bottom-5">
+      <div className="page-list page-bg positon-relative" id="page-list">
+
+        {/* 顶部 */}
+        <div className="border-box top-wrapper padding-bottom-5">
+          <div className="bg-color-theme padding-left-15 padding-right-15 ">
           <WhiteSpace/>
           <SearchCard 
             showAddress={false}
@@ -131,6 +168,7 @@ class Discovery extends Component {
           >
             </SearchCard>
           <WhiteSpace size="lg"/>
+          {/* tab */}
           <div className="list-tab vr-tabs position-relative">
             <Tabs
               tabBarBackgroundColor='#ED6C2D'
@@ -149,16 +187,74 @@ class Discovery extends Component {
               onTabClick={(tab,index)=>this.handelTabClick(tab,index)}
             >
             </Tabs>
-            <div className="down-button text-color-fff bg-color-theme flex align-items-center justify-content-center">
+           
+            <div 
+            onClick={this.onOpenChange}
+            className="down-button text-color-fff bg-color-theme flex align-items-center justify-content-center">
               >
             </div>
           </div>
+          
+          </div>
+         {/* 下拉nav */}
+         {
+          this.state.open? 
+            <div className="downpanal-nav bg-color-white flex">
+              <div
+              style={{
+                flex:'0 0 130px'
+              }}
+              className="panale-left">
+                <ul className="firstList">
+                  {this.state.navData.map((el,index)=>
+                    <li 
+                    onClick={this.handleFirNavClick.bind(this,index)}
+                    className={`${this.state.activeFirList===index ?'active':''} item flex align-items-center justify-content-between`} 
+                    key={el.id}>
+                      <span className="lable text-color-666 font-size-15">{el.name}</span>
+                      <span className="number font-size-11 text-color-999">{el.num}</span>
+                    </li>
+                  )}
+                </ul>
+              </div>
+              <div className="panle-right flex-1">
+                <div>
+                  {this.state.navData.map((el,index)=>
+                  this.state.activeFirList===index && 
+                  <ul className="secList" key={index}>
+                        {
+                          el.children.map((e,i)=>
+                          <li
+                          onClick={this.handleSecNavClick.bind(this,i)}
+                          key={i} className={`${this.state.activeSecList===i?'active':''}  item flex align-items-center justify-content-between`}>
+                            <div className="flex align-items-center">
+                              <img width="44" height="32" src={e.icon||defaultImg} alt=""/>
+                              <span className="margin-left-10 lable text-color-666 font-size-15">{e.name}</span>
+                            </div>
+                            <span className="number font-size-11 text-color-999">{e.num}</span>
+                          </li>)
+                        }
+                    </ul>
+                    )
+                  }
+                </div>
+              </div>
+            </div>
+            :null
+         }
+         
         </div>
+        
+        {/* mask */}
+        {
+          this.state.open ? <div className="menu-mask" onClick={this.onOpenChange} /> : null
+        }
        
+
       </div >
     );
   }
 }
-export default Discovery;
+export default BussList;
 
 // export default Discovery;
--- src/services/scenter.js
+++ src/services/scenter.js
@@ -12,4 +12,11 @@ API.getAttentionList=(params)=> {
   return proxyRequest.get(baseUrl+'/foodie/search', params)
 };
 
+
+//列表页分类
+API.getStoreClassList=(params)=> {
+  return proxyRequest.get(baseUrl+'/store/class/list', params)
+};
+
+
 export default API;
